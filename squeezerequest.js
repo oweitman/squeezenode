df453802e5bfa7a4d4bd38c0c4a7f05a9acd096e
pssc <phillip.camp+git@gmail.com>
2017-08-02 17:40:22 +0100

    serialised access to sqs queue for communication

--- squeezerequest.js
+++ squeezerequest.js
@@ -24,6 +24,8 @@
 
 var jayson = require('jayson');
 var inherits = require('super');
+var AWS;
+
 
 function SqueezeRequest(address, port, username, password) {
   
@@ -34,14 +36,9 @@ function SqueezeRequest(address, port, username, password) {
     var jsonrpc = this.address + ':' + this.port + '/jsonrpc.js';
     var client = jayson.client.http(jsonrpc);
     client.options.version = 1;
-
-    // Add a header for basic authentication if a username and password are given
-
-    if (username && password) {
-        if (! client.options.headers)
-            client.options.headers = {};
-        client.options.headers['Authorization'] = formatBasicHeader(username, password);
-    }
+    this.queue = [];
+    this.id = "squeezenode."+process.pid;
+    var that = this;
 
     function handle(err, reply, callback) {
         var result = {};
@@ -57,14 +54,110 @@ function SqueezeRequest(address, port, username, password) {
             callback(result);
     }
 
-    this.request = function (player, params, callback) {
-        var finalParams = [];
-        finalParams.push(player);
-        finalParams.push(params);
-        client.request('slim.request', finalParams, null, function (err, reply) {
+    if (port == 'aws') {
+        AWS = require('aws-sdk');
+	this.request = request_sqs;
+	this.sqs = new AWS.SQS({region: 'us-west-2'});
+	this.sendq = { QueueUrl: password.send, MessageGroupId: this.id };
+	this.recvq = { QueueUrl: password.recv, WaitTimeSeconds: 0, MaxNumberOfMessages: 10};
+	this.reciving = true
+	this.sqs.receiveMessage(that.recvq, function (err, reply) {
+		that.recvq.MaxNumberOfMessages = 1;
+		that.recvq.WaitTimeSeconds = 20;
+		if (reply.Messages) {
+			var msg = reply.Messages.pop();
+			while (msg) {
+				var deleteParams = {
+					QueueUrl: that.recvq.QueueUrl,
+					ReceiptHandle: msg.ReceiptHandle
+				};
+				that.sqs.deleteMessage(deleteParams, function(err, data) {});
+				msg = reply.Messages.pop();
+			}
+		}
+		request_sqs_dispatch_queue();
+	}); //Drain Q.
+    } else {
+	this.request = request_rpc;
+
+	// Add a header for basic authentication if a username and password are given
+	if (username && password) {
+		if (! client.options.headers)
+			client.options.headers = {};
+		client.options.headers['Authorization'] = formatBasicHeader(username, password);
+	}
+    }
+
+    function request_rpc(player, params, callback) {
+        client.request('slim.request', [player, params], that.id, function (err, reply) {
             handle(err, reply, callback);
         });
-    };
+    }
+
+    function request_sqs_dispatch(player, params, callback) {
+	that.sendq['MessageBody'] = JSON.stringify({"params":[player, params],"id":that.id,"version":"1.0","method":"slim.request"}); // FIXME id not on pid but an uuid? for instrance from env?
+	that.sendq['MessageDeduplicationId'] = that.sendq.MessageGroupId+process.hrtime();
+	//console.log("send ",that.sendq['MessageBody']);
+	that.sqs.sendMessage(that.sendq, function(err, data) {
+		  if (err) {
+			handle(err, data, callback);
+			//console.log("send error ",err);
+		  } else {
+			// successful response - callback for reply
+			that.reciving = true;
+			that.sqs.receiveMessage(that.recvq, function (err, reply) {
+				//console.log("reply data ",reply);
+				var message_data = {};
+				if (reply.Messages) {
+					//id check?
+					message_data = reply.Messages.pop();
+					//delete and drain?
+					var msg = message_data;
+					message_data = JSON.parse(message_data.Body);
+					//console.log("reply ",message_data);
+					while (msg) {
+						var deleteParams = {
+							QueueUrl: that.recvq.QueueUrl,
+							ReceiptHandle: msg.ReceiptHandle
+						};
+						// FIXME delete error
+						that.sqs.deleteMessage(deleteParams, function(err, data) {
+							if (err) {
+								//console.log("Delete Error", err);
+							} else {
+								//console.log("Message Deleted", data);
+							}
+						});
+						msg = reply.Messages.pop();
+					}
+				} else {
+					//console.dir("no msgs ",err);
+					err = "No reply message recvied";
+				}
+				handle(err, message_data, callback);
+				request_sqs_dispatch_queue();
+			});
+		  }
+	});
+    }
+
+    // We need to serialise requests and responces for queue use
+    function request_sqs(player, params, callback) {
+	if (that.reciving) {
+		that.queue.push([player, params, callback]);
+	} else {
+		request_sqs_dispatch(player, params, callback);
+	}
+    }
+
+    function request_sqs_dispatch_queue() {
+	    var args = that.queue.pop();
+	    if (args) {
+		    request_sqs_dispatch.apply(null,args);
+	    } else {
+		    that.reciving = false;
+	    }
+    }
 }
 
 /**
